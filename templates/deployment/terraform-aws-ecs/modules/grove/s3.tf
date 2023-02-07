e30f1bc02dd9a232059331a28bb0470ead7ab5e8
Jonas Plum <git@cugu.eu>
2023-02-07 14:08:40 +0100

    Migration of AWS ecs deployment from internal fork

---
+++ templates/deployment/terraform-aws-ecs/modules/grove/s3.tf
@@ -0,0 +1,35 @@
+# Create the bucket for Grove to output collected logs to.
+resource "aws_s3_bucket" "logs" {
+  bucket = "${var.name}-output"
+}
+
+resource "aws_s3_bucket_acl" "logs_bucket_acl" {
+  bucket = aws_s3_bucket.logs.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_versioning" "artifact_bucket_versioning" {
+  bucket = aws_s3_bucket.logs.bucket
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "logs_bucket_enc_config" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "logs" {
+  bucket                  = aws_s3_bucket.logs.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
