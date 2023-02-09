a50ed81ca9c8f5cc61a980ae9ebd189954584c2d
melissahardware <mhardware@hashicorp.com>
2023-02-09 10:18:29 -0500

    add atlassian, workday, okta and zoom connectors

---
+++ grove/connectors/zoom/__init__.py
@@ -0,0 +1,1 @@
+"""Zoom connectors for Grove."""
---
+++ grove/connectors/zoom/operationlogs.py
@@ -0,0 +1,65 @@
+"""Zoom activity log connector for grove."""
+
+from datetime import datetime, timedelta
+
+from grove.connectors import BaseConnector
+from grove.connectors.zoom import api
+from grove.constants import REVERSE_CHRONOLOGICAL
+from grove.exceptions import NotFoundException
+
+DATESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+
+class Connector(BaseConnector):
+    NAME = "zoom_operationlogs"
+    POINTER_PATH = "time"
+    LOG_ORDER = REVERSE_CHRONOLOGICAL
+
+    @property
+    def client_id(self):
+        """Fetches the Zoom ClientID from the configuration.
+
+        This is required as this is a third authentication element required by Zoom.
+        """
+        try:
+            return self.configuration.client_id
+        except AttributeError:
+            return None
+
+    def collect(self):
+        """Collects all logs from the Zoom Activity API."""
+        client = api.Client(
+            identity=self.identity,
+            client_id=self.client_id,
+            key=self.key,
+        )
+
+        # Use the refresh token to generate a temporary access token.
+        client.get_access_token()
+
+        cursor = None
+
+        # If no pointer is stored then a previous run hasn't been performed, so set the
+        # pointer to a day ago.
+        now = datetime.utcnow().strftime(DATESTAMP_FORMAT)
+        try:
+            _ = self.pointer
+        except NotFoundException:
+            self.pointer = (datetime.utcnow() - timedelta(days=1)).strftime(
+                DATESTAMP_FORMAT
+            )
+
+        # Get log data from the upstream API. A "from" and "to" datetime query parameters
+        # are required. This API only gets data from a YYYYMMDD date range.
+        while True:
+            log = client.get_operationlogs(
+                from_date=self.pointer, to_date=now, cursor=cursor
+            )
+
+            # Save this batch of log entries.
+            self.save(log.entries)
+
+            # Check if we need to continue paging.
+            cursor = log.cursor
+            if cursor is None:
+                break
