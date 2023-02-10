3dfe1f8b7943a3601e7cd5e9a50491d67604c7f7
Melissa Hardware <83655400+melissahardware@users.noreply.github.com>
2023-02-10 06:29:24 -0500

    add atlassian, workday, okta and zoom connectors (#8)
    add atlassian, workday, okta and zoom connectors

---
+++ grove/connectors/workday/__init__.py
@@ -0,0 +1,1 @@
+"""Workday connectors for Grove."""
---
+++ grove/connectors/zoom/activities.py
@@ -0,0 +1,67 @@
+"""Zoom activity log connector for grove."""
+
+from datetime import datetime, timedelta
+
+from grove.connectors import BaseConnector
+from grove.connectors.zoom import api
+from grove.constants import REVERSE_CHRONOLOGICAL
+from grove.exceptions import NotFoundException
+
+DATESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
+
+
+class Connector(BaseConnector):
+    NAME = "zoom_activities"
+    POINTER_PATH = "time"
+    LOG_ORDER = REVERSE_CHRONOLOGICAL
+
+    @property
+    def client_id(self):
+        """Fetches the Zoom ClientID from the configuration.
+
+        This is required as this is a third authentication element required by Zoom.
+        """
+        try:
+            return self.configuration.client_id
+        except AttributeError:
+            return None
+
+    def collect(self):
+        """Collects all logs from the Zoom Activity API."""
+        client = api.Client(
+            identity=self.identity,
+            client_id=self.client_id,
+            key=self.key,
+        )
+
+        # Use the refresh token to generate a temporary access token.
+        client.get_access_token()
+
+        cursor = None
+
+        # If no pointer is stored then a previous run hasn't been performed, so set the
+        # pointer to a day ago. This API uses a YYYYMMDD date range. We can input using
+        # RFC 3339 format because the API converts the to and from to YYYYMMDD.
+        now = datetime.utcnow().strftime(DATESTAMP_FORMAT)
+        try:
+            _ = self.pointer
+        except NotFoundException:
+            self.pointer = (datetime.utcnow() - timedelta(days=1)).strftime(
+                DATESTAMP_FORMAT
+            )
+
+        # Get log data from the upstream API. A "from" and "to" datetime query parameters
+        # are required. This API only gets data from the last 24 hours and should only
+        # run once a day or else we will get duplicated data each run
+        while True:
+            log = client.get_activities(
+                from_date=self.pointer, to_date=now, cursor=cursor
+            )
+
+            # Save this batch of log entries.
+            self.save(log.entries)
+
+            # Check if we need to continue paging.
+            cursor = log.cursor
+            if cursor is None:
+                break
