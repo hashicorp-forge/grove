2de90443b03ab32152791ff593fcc12cb7c8eae9
Peter Adkins <74542596+hcpadkins@users.noreply.github.com>
2023-12-11 15:55:46 +0000

    Increment version (#42)

--- grove/__about__.py
+++ grove/__about__.py
@@ -1,6 +1,6 @@
 """Grove metadata."""
 
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 __title__ = "grove"
 __license__ = "Mozilla Public License 2.0"
 __copyright__ = "Copyright 2023 HashiCorp, Inc."
