95b178e75b256d683c90f7fb82417fed500b417d
Peter Adkins <74542596+hcpadkins@users.noreply.github.com>
2024-08-28 12:52:35 +0100

    Remove fixture used for testing

--- grove.py
+++
@@ -1,8 +0,0 @@
-#!/home/jess/.venv/bin/python
-# -*- coding: utf-8 -*-
-import re
-import sys
-from grove.entrypoints.local_process import entrypoint
-if __name__ == '__main__':
-    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.exit(entrypoint())
