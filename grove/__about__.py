2fc92f54061b5a27967ee5548ef05ae7d9193f5e
Peter Adkins <padkins@hashicorp.com>
2025-01-07 15:50:09 +0000

    Updated licence dates.

--- grove/__about__.py
+++ grove/__about__.py
@@ -3,4 +3,4 @@
 __version__ = "2.0.0"
 __title__ = "grove"
 __license__ = "Mozilla Public License 2.0"
-__copyright__ = "Copyright 2024 HashiCorp, Inc."
+__copyright__ = "Copyright 2025 HashiCorp, Inc."
