b9616459d379d5e237c964e3b668244be7639373
Peter Adkins <padkins@hashicorp.com>
2025-05-22 14:48:28 +0100

    Version bump for release.

--- grove/__about__.py
+++ grove/__about__.py
@@ -1,6 +1,6 @@
 """Grove metadata."""
 
-__version__ = "2.1.1"
+__version__ = "2.2.0"
 __title__ = "grove"
 __license__ = "Mozilla Public License 2.0"
 __copyright__ = "Copyright 2025 HashiCorp, Inc."
