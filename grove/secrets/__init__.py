fdb99609026d4285ff108d5645c76175b9774452
Abhishek <86008917+abhihashi@users.noreply.github.com>
2026-08-19 09:51:26 -0400

    security: remove clear-text exception logging in secrets backend (alert 53) (#134)

--- grove/secrets/__init__.py
+++ grove/secrets/__init__.py
@@ -66,14 +66,14 @@ class BaseSecret(abc.ABC):
                     exc_info=False,
                 )
                 continue
-            except (AccessException, IndexError) as err:
+            except (AccessException, IndexError):
                 self.logger.error(
                     "Unable to get secret for connector, skipping",
                     extra={
                         "document": configuration.name,
                         "field": field,
-                        "exception": err,
                     },
+                    exc_info=False,
                 )
                 continue
 
